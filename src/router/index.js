67fd92e462a8210053a95bb72008517859152347
gtobio11 <gtobio11@gmail.com>
2020-01-09 14:38:32 +0900

    [ADD] post data, get post post data

--- src/router/index.js
+++ src/router/index.js
@@ -2,4 +2,51 @@ module.exports = function(app, fs) {
     app.get("/", function(req, res) {
         res.render("index.html")
     })
+
+    app.get("/getPosts", function(req, res) {
+        fs.readFile(__dirname + "/../data/posts.json", "utf-8", function(err, data) {
+            let postsData = JSON.parse(data);
+            res.send(postsData);
+        })
+    });
+
+    app.get("/getPosts/:category", function(req, res) {
+        fs.readFile(__dirname + "/../data/posts.json", "utf-8", function(err, data) {
+            let postsData = JSON.parse(data);
+            console.log(req.params.category);
+            if(req.params.category) {
+                postsData = postsData.filter(post => post.category === req.params.category)
+            }
+            res.send(postsData);
+        })
+    });
+
+    app.post("/createPost", function(req, res) {
+        fs.readFile(__dirname + '/../data/posts.json', "utf-8", function(readErr, readData) {
+            let postsData = JSON.parse(readData);
+            const { title, description, category = "" } = req.body;
+            if(title && description){
+                postsData = [...postsData, {
+                    title, 
+                    description, 
+                    category
+                }];
+                
+            
+                fs.write(
+                    __dirname + '/../data/posts.json', 
+                    JSON.stringify(postsData, null, "\t"),
+                    "utf-8",
+                    function(writeErr, writeData){
+                        if(writeErr) {
+                            console.log(writeErr);
+                            res.status(500).send();
+                        }
+                        res.status(200).send(postsData);
+                    })
+            } else {
+                res.status(400).send(); 
+            }
+        })
+    })
 }
