6c45cefd35e47a5567301c7990b948923dd660dc
gtobio11 <gtobio11@gmail.com>
2020-01-10 16:08:35 +0900

    [ADD] post delete api

--- src/router/index.js
+++ src/router/index.js
@@ -25,12 +25,13 @@ module.exports = function(app, fs) {
     });
 
     app.post("/createPost", function(req, res) {
-        fs.readFile(__dirname + '/../data/posts.json', "utf-8", function(readErr, readData) {
-            let postsData = JSON.parse(readData);
-            const { title, description, category = "" } = req.body;
-            if(title && description){
-                let postId = 0;
+        const { title, description, category = "" } = req.body;
+        if(title && description){
+            fs.readFile(__dirname + '/../data/posts.json', "utf-8", function(readErr, readData) {
+                let postsData = JSON.parse(readData);
+                let postId = 1;
                 if(postsData.length !== 0) postId = postsData[postsData.length - 1].postId + 1;
+
                 postsData = [...postsData, {
                     postId: postId,
                     date : dateFormat(new Date),
@@ -49,11 +50,51 @@ module.exports = function(app, fs) {
                             console.log(writeErr);
                             res.status(500).send();
                         }
-                        res.status(200).send(postsData);
+                        res.status(201).send(postsData);
                     })
-            } else {
-                res.status(400).send(); 
-            }
-        })
+            })
+        } else {
+            res.status(400).send(); 
+        }
+    })
+
+    app.delete("/deletePost", function(req, res) {
+        const { postId: willDeletePostId} = req.body;
+        if(willDeletePostId) {
+            fs.readFile(__dirname + '/../data/posts.json', "utf-8", function(readErr, readData) {
+                let postsData = JSON.parse(readData);
+                let isDeleted = false;
+
+                console.log(postsData, willDeletePostId);
+                postsData = postsData.filter((postData) => {
+                    console.log(postData, willDeletePostId);
+                    if(postData.postId === willDeletePostId) {
+                        isDeleted = true;
+                        return false;
+                    }
+
+                    return true;
+                })
+                
+                if(!isDeleted) {
+                    res.status(404).send({message: "Nonexistent PostId"});
+                } else {
+                    fs.writeFile(
+                        __dirname + '/../data/posts.json', 
+                        JSON.stringify(postsData, null, "\t"),
+                        "utf-8",
+                        function(writeErr, writeData){
+                            if(writeErr) {
+                                console.log(writeErr);
+                                res.status(500).send();
+                            }
+                            res.status(200).send(postsData);
+                        })
+                }
+            })
+        }
+        else {
+            res.status(400).send();
+        }
     })
 }
