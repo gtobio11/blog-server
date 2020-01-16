720a86e5b8a2e515bb4eb1d2c0433dbe4341b0c2
gtobio11 <gtobio11@gmail.com>
2020-01-16 18:15:07 +0900

    [FIX] date format mistake

--- src/util/dateFormat.js
+++ src/util/dateFormat.js
@@ -1,4 +1,4 @@
 const zeroFormat = require('./zeroFormat');
 module.exports = function(dateObj) {
-    return `${dateObj.getYear() + 1890}.${zeroFormat(dateObj.getMonth() + 1, 2)}.${zeroFormat(dateObj.getDate(),2)} ${zeroFormat(dateObj.getHours(),2)}:${zeroFormat(dateObj.getMinutes(),2)}`
+    return `${dateObj.getYear() + 1900}.${zeroFormat(dateObj.getMonth() + 1, 2)}.${zeroFormat(dateObj.getDate(),2)} ${zeroFormat(dateObj.getHours(),2)}:${zeroFormat(dateObj.getMinutes(),2)}`
 }
